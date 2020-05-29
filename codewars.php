9db1f8e1edf3cb5f7309b3842e1346fe033836b4
Oleg Vishinsky <vishinsky.oleg@gmail.com>
2020-05-29 07:59:36 +0300

    Commit

--- codewars.php
+++ codewars.php
@@ -153,9 +153,7 @@ function generateHashtag1($str) {
 
 
 function generateHashtag_adv($str) {
-
     $str = '#' . str_replace(' ', '', ucwords($str));
-
     return (strlen($str) > 140 || strlen($str) == 1) ? false : $str;
 }
 
